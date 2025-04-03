fmyuan/E3SM

Machine File Modifications: update 'cades' of ornl-ccsi

cime_config/machines/cmake_macros/gnu_cades.cmake
@@ -1,3 +1,30 @@
-string(APPEND CMAKE_Fortran_FLAGS " -O -fno-range-check")
+set(CXX_LINKER "CXX")
+string(APPEND FC_AUTO_R8 " -fdefault-real-8")
+string(APPEND FFLAGS " -O -fconvert=big-endian -ffree-line-length-none -ffixed-line-length-none -fno-range-check")
+string(APPEND FIXEDFLAGS " -ffixed-form")
+string(APPEND FREEFLAGS " -ffree-form")
+set(HDF5_PATH "$ENV{HDF5_PATH}")
+set(NETCDF_PATH "$ENV{NETCDF_PATH}")
+set(NETCDF_C_PATH "$ENV{NETCDF_PATH}")
+set(NETCDF_FORTRAN_PATH "$ENV{NETCDF_PATH}")
+set(PNETCDF_PATH "$ENV{PNETCDF_PATH}")
+set(LAPACK_LIBDIR "$ENV{BLASLAPACK_LIBDIR}")
+if (NOT MPILIB STREQUAL mpi-serial)
+  string(APPEND CFLAGS " -I/${NETCDF_C_PATH}/include -I/${HDF5_PATH}/include")
+  string(APPEND FFLAGS " -I/${NETCDF_FORTRAN_PATH}/include -I/${NETCDF_C_PATH}/include -I/${HDF5_PATH}/include")
+  string(APPEND SLIBS " -L${HDF5_PATH}/lib -Wl,-rpath=${HDF5_PATH}/lib -lhdf5_hl_fortran -lhdf5_fortran -lhdf5_hl -lhdf5")
+  string(APPEND SLIBS " -L${NETCDF_C_PATH}/lib -Wl,-rpath=${NETCDF_C_PATH}/lib -lnetcdf")
+  string(APPEND SLIBS " -L${NETCDF_FORTRAN_PATH}/lib -Wl,-rpath=${NETCDF_FORTRAN_PATH}/lib -lnetcdff")
+endif()
+if (MPILIB STREQUAL mpi-serial)
+  string(APPEND SLIBS " -L$ENV{NETCDF_PATH}/lib -Wl,-rpath=$ENV{NETCDF_PATH}/lib -lnetcdff -lnetcdf")
+endif()
+string(APPEND SLIBS " -L${LAPACK_LIBDIR} -Wl,-rpath=${LAPACK_LIBDIR} -lblas -llapack")
+string(APPEND CXX_LIBS " -lstdc++")
+set(MPICC "mpicc")
 set(MPICXX "mpic++")
-set(SCXX "gcpp")
+set(MPIFC "mpif90")
+set(SCC "gcc")
+set(SCXX "g++")
+set(SFC "gfortran")
+set(SUPPORTS_CXX "TRUE")

ORNLngee/E3SM_ORNL_IM

cime_config/machines/cmake_macros/gnu_cades.cmake
@@ -1,3 +1,32 @@
-string(APPEND CMAKE_Fortran_FLAGS " -O -fno-range-check")
+set(CXX_LINKER "CXX")
+set(PIO_FILESYSTEM_HINTS "lustre")
+# the following may not be needed for recent E3SM
+string(APPEND FC_AUTO_R8 " -fdefault-real-8")
+string(APPEND FFLAGS " -O -fconvert=big-endian -ffree-line-length-none -ffixed-line-length-none -fno-range-check")
+string(APPEND FIXEDFLAGS " -ffixed-form")
+string(APPEND FREEFLAGS " -ffree-form")
+set(HDF5_PATH "$ENV{HDF5_PATH}")
+set(NETCDF_PATH "$ENV{NETCDF_PATH}")
+set(NETCDF_C_PATH "$ENV{NETCDF_C_PATH}")
+set(NETCDF_FORTRAN_PATH "$ENV{NETCDF_FORTRAN_PATH}")
+set(PNETCDF_PATH "$ENV{PNETCDF_PATH}")
+set(LAPACK_LIBDIR "$ENV{BLASLAPACK_LIBDIR}")
+if (NOT MPILIB STREQUAL mpi-serial)
+  string(APPEND CFLAGS " -I/${NETCDF_C_PATH}/include -I/${HDF5_PATH}/include")
+  string(APPEND FFLAGS " -I/${NETCDF_FORTRAN_PATH}/include -I/${NETCDF_C_PATH}/include -I/${HDF5_PATH}/include")
+  string(APPEND SLIBS " -L${HDF5_PATH}/lib -Wl,-rpath=${HDF5_PATH}/lib -lhdf5_hl_fortran -lhdf5_fortran -lhdf5_hl -lhdf5")
+  string(APPEND SLIBS " -L${NETCDF_C_PATH}/lib -Wl,-rpath=${NETCDF_C_PATH}/lib -lnetcdf")
+  string(APPEND SLIBS " -L${NETCDF_FORTRAN_PATH}/lib -Wl,-rpath=${NETCDF_FORTRAN_PATH}/lib -lnetcdff")
+endif()
+if (MPILIB STREQUAL mpi-serial)
+  string(APPEND SLIBS " -L$ENV{NETCDF_PATH}/lib -Wl,-rpath=$ENV{NETCDF_PATH}/lib -lnetcdff -lnetcdf")
+endif()
+string(APPEND SLIBS " -L${LAPACK_LIBDIR} -Wl,-rpath=${LAPACK_LIBDIR}")
+string(APPEND CXX_LIBS " -lstdc++")
+set(MPICC "mpicc")
 set(MPICXX "mpic++")
-set(SCXX "gcpp")
+set(MPIFC "mpif90")
+set(SCC "gcc")
+set(SCXX "g++")
+set(SFC "gfortran")
+set(SUPPORTS_CXX "TRUE")