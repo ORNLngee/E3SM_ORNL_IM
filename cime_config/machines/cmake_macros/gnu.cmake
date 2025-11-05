fmyuan/E3SM

On ubuntu or alike Linux OS, compiling system cannot pick up those CMAKE options in components/cmake/common_setup.cmake. So move those into cime_config/machines/cmake_macros/

cime_config/machines/cmake_macros/gnu.cmake
@@ -31,6 +31,19 @@ if (COMP_NAME STREQUAL csm_share)
 endif()
 string(APPEND CMAKE_Fortran_FORMAT_FIXED_FLAG " -ffixed-form")
 string(APPEND CMAKE_Fortran_FORMAT_FREE_FLAG " -ffree-form")
+set(ALQUIMIA_PATH $ENV{ALQUIMIA_PATH})
+set(PFLOTRAN_PATH $ENV{PFLOTRAN_PATH}/src/pflotran)
+if (ALQUIMIA_PATH AND PFLOTRAN_PATH)
+  if (COMP_NAME STREQUAL "elm")
+    string(APPEND CPPDEFS " -DELM_USE_ALQUIMIA")
+    string(APPEND CMAKE_C_FLAGS " -I${ALQUIMIA_PATH}/include")
+    string(APPEND CMAKE_CXX_FLAGS " -I${ALQUIMIA_PATH}/include")
+    string(APPEND CMAKE_Fortran_FLAGS " -I${ALQUIMIA_PATH}/include")
+  endif()
+  if (COMP_NAME STREQUAL "cpl")
+    string(APPEND CMAKE_EXE_LINKER_FLAGS " -Wl,-rpath,${ALQUIMIA_PATH}/lib -L${ALQUIMIA_PATH}/lib -lalquimia")    
+  endif()
+endif()
 set(MPICC "mpicc")
 set(MPICXX "mpicxx")
 set(MPIFC "mpif90")

ORNLngee/E3SM_ORNL_IM

cime_config/machines/cmake_macros/gnu.cmake
@@ -31,6 +31,19 @@ if (COMP_NAME STREQUAL csm_share)
 endif()
 string(APPEND CMAKE_Fortran_FORMAT_FIXED_FLAG " -ffixed-form")
 string(APPEND CMAKE_Fortran_FORMAT_FREE_FLAG " -ffree-form")
+set(ALQUIMIA_PATH $ENV{ALQUIMIA_PATH})
+set(PFLOTRAN_PATH $ENV{PFLOTRAN_PATH}/src/pflotran)
+if (ALQUIMIA_PATH AND PFLOTRAN_PATH)
+  if (COMP_NAME STREQUAL "elm")
+    string(APPEND CPPDEFS " -DUSE_ALQUIMIA_LIB")
+    string(APPEND CMAKE_C_FLAGS " -I${ALQUIMIA_PATH}/include")
+    string(APPEND CMAKE_CXX_FLAGS " -I${ALQUIMIA_PATH}/include")
+    string(APPEND CMAKE_Fortran_FLAGS " -I${ALQUIMIA_PATH}/include")
+  endif()
+  if (COMP_NAME STREQUAL "cpl")
+    string(APPEND CMAKE_EXE_LINKER_FLAGS " -Wl,-rpath,${ALQUIMIA_PATH}/lib -L${ALQUIMIA_PATH}/lib -lalquimia")    
+  endif()
+endif()
 set(MPICC "mpicc")
 set(MPICXX "mpicxx")
 set(MPIFC "mpif90")